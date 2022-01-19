3f105b6c5c23c01ede60019716cd43b8f5a0f891
Ryan Bode <rbcrusher88@gmail.com>
2022-01-19 17:54:44 -0500

    feat: base auto shooting system

--- src/main/java/frc/robot/commands/AutoShoot.java
+++ src/main/java/frc/robot/commands/AutoShoot.java
@@ -2,6 +2,8 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+import frc.robot.Robot;
 
 public class AutoShoot extends CommandBase {
 
@@ -12,7 +14,7 @@ public class AutoShoot extends CommandBase {
     private boolean m_finished = false;
 
     public AutoShoot() {
-        // TODO: Add requirements.
+        this.addRequirements(Robot.shooter);
     }
 
     @Override
