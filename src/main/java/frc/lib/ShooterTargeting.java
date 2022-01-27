54cf4927227cc0f240711653d04a12c1173ddca2
Ryan Bode <rbcrusher88@gmail.com>
2022-01-27 15:47:08 -0500

    feat: targeting math

--- src/main/java/frc/lib/ShooterTargeting.java
+++ src/main/java/frc/lib/ShooterTargeting.java
@@ -1,9 +1,18 @@
 package frc.lib;
 
 import frc.robot.Constants;
+import frc.robot.Robot;
 
 public class ShooterTargeting {
 
+    // -b squared plus c squared = a squared
+    // C is the TD from limelight
+    // B is the height of the goal - height of limelight
+    // A is the distance between limelight and base of goal
+    public static double findZ() {
+        return (Math.sqrt(-Math.pow((104-Constants.Targeting.kLimelightHeightFromGround), 2) + Math.pow(Robot.limelight.getDistance(), 2)));
+    }
+    
     // Calculate optimal angle
     public static double calculateAngle() {
         return 0.0;
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,9 +1,10 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
 
 public class Intake extends SubsystemBase {
-    
+      
     public enum IntakeMotorState {
       ON,
       OFF,
