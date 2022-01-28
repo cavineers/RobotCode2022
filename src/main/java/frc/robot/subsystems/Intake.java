62a8fa9943c6b0819eb2d593bd9bcc0cf0c52535
Ryan Bode <rbcrusher88@gmail.com>
2022-01-28 12:39:57 -0500

    feat: update intake motor

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,16 +1,21 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Intake extends SubsystemBase {
-      
-    public enum IntakeMotorState {
-      ON,
-      OFF,
-      REVERSED
-    }
-  
-    /** Creates a new Intake. */
-    public Intake() {}
+    
+  private CANSparkMax m_lowerBrushMotor = new CANSparkMax(Constants.Intake.IntakeLowerBrush, MotorType.kBrushless);
+
+  public enum IntakeMotorState {
+    ON,
+    OFF,
+    REVERSED
+  }
+
+  /** Creates a new Intake. */
+  public Intake() {}
 }
