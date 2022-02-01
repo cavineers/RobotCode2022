366b822db82cabe813f83f6e9980e188fb157c94
Ryan Bode <rbcrusher88@gmail.com>
2022-02-01 10:17:10 -0500

    fix: motor status on climber systems

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,47 +10,23 @@ import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
 
-  private CANSparkMax m_shooterMotor = new CANSparkMax(Constants.Climber.ClimberMotor, MotorType.kBrushless);
+  private CANSparkMax m_climberElevatorOne = new CANSparkMax(Constants.Climber.ClimberMotor, MotorType.kBrushless);
   
-  private CANSparkMax m_shooterMotorTwo = new CANSparkMax(Constants.Climber.ClimberMotorTwo, MotorType.kBrushless);
+  private CANSparkMax m_climberElevatorTwo = new CANSparkMax(Constants.Climber.ClimberMotorTwo, MotorType.kBrushless);
 
-  private CANSparkMax m_shooterMotorThree = new CANSparkMax(Constants.Climber.ClimberMotorThree, MotorType.kBrushless);
+  private CANSparkMax m_climberAngleOne = new CANSparkMax(Constants.Climber.ClimberMotorThree, MotorType.kBrushless);
   
-  private CANSparkMax m_shooterMotorFour = new CANSparkMax(Constants.Climber.ClimberMotorFour, MotorType.kBrushless);
-  
-  private CANSparkMax climberMotor;
-
-  private CANSparkMax climberMotorTwo;
-
-  private CANSparkMax climberMotorThree;
-
-  private CANSparkMax climberMotorFour;
+  private CANSparkMax m_climberAngleTwo = new CANSparkMax(Constants.Climber.ClimberMotorFour, MotorType.kBrushless);
   
   public enum ClimberMotorStatus {
     ON,
     OFF
   }
-  
-  public enum ClimberMotorTwoStatus {
-    ON,
-    OFF
-  }
-  public enum ClimberMotorThreeStatus {
-    ON,
-    OFF
-  }
-  
-  public enum ClimberMotorFourStatus {
-    ON,
-    OFF
-  }
 
-
-    /** Creates a new ExampleSubsystem. */
-    public Climber() {}
-    
-    @Override
-    public void periodic() {
-      // This method will be called once per scheduler run
-    }
+  public Climber() {}
+  
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
