9d53790298c36d0ff5cbcde1e174d455f45af8d2
HaBr24 <habr24@spaldinghs.org>
2022-02-03 16:25:50 -0500

    Added sensor code
    Sensored deez nuts

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,10 +1,11 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.revrobotics.CANSparkMax;
 
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Robot;
 
 
 public class Intake extends SubsystemBase {
@@ -49,6 +50,56 @@ public void setMotorState(IntakeMotorState state) {
         case REVERSED:
             // Reversed
             this.m_intakeMotor.set(-1.0);
+<<<<<<< Updated upstream
+=======
+            break;
+        default:
+            this.setMotorState(IntakeMotorState.OFF);
+    }
+}
+
+/**
+ * Get the current intake state.
+ * @return intake state
+ */
+public IntakeMotorState getMotorTwoState() {
+    // return the current motor state
+    return this.m_currentMode;
+}
+public CANSparkMax m_intakeMotorTwo = new CANSparkMax(Constants.Intake.kIntakeID, null);
+
+// Current intake mode
+public IntakeMotorState m_currentState = IntakeMotorState.OFF;
+
+/**
+ * Intake constructor.
+ */
+public void DropIntake() {
+    this.setMotorTwoState(IntakeMotorState.OFF);
+}
+/**
+ * Set the desired intake state.
+ * @param state wanted intake state
+ */
+public void setMotorTwoState(IntakeMotorState state) {
+    // set the current state
+    this.m_currentMode = state;
+    
+
+    // set motor state
+    switch (state) {
+        case ON:
+            // On
+            this.m_intakeMotor.set(.50);
+            break;
+        case OFF:
+            // Off
+            this.m_intakeMotor.set(0);
+            break;
+        case REVERSED:
+            // Reversed
+            this.m_intakeMotor.set(-0.50);
+>>>>>>> Stashed changes
             break;
         default:
             this.setMotorState(IntakeMotorState.OFF);
@@ -62,5 +113,20 @@ public void setMotorState(IntakeMotorState state) {
 public IntakeMotorState getMotorState() {
     // return the current motor state
     return this.m_currentMode;
+    
 }
+private DigitalInput m_sensorOne = new DigitalInput(Constants.Intake.kIntakeSensor);
+public void toggleFeeder() {
+        Robot.Intake.addInfo("ToggleIntake", "Intake Toggle");
+        if (Robot.Intake.getIntakeMotorState() == IntakeMotorState.OFF) {
+            Robot.Intake.setIntakeMotorState(IntakeMotorState.ON);
+        } else {
+            Robot.Intake.setIntakeMotorState(IntakeMotorState.OFF);
+            }
+
+
+}
+public boolean getSensorOneState() {
+        return !m_sensorOne.get();
+    }
 }
