336db891147b6335ad3142a9d4c79fdf71901f44
POODLEMASTER1 <91638186+POODLEMASTER1@users.noreply.github.com>
2022-02-07 16:51:00 -0500

    We are partly done
    we are part done

---
+++ src/main/java/frc/robot/commands/ToggleSensor.java
@@ -0,0 +1,14 @@
+package frc.robot.commands;
+import frc.robot.Constants;
+import frc.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ToggleSensor extends CommandBase {
+
+        private DigitalInput m_sensorOne = new DigitalInput(Constants.Intake.kIntakeSensor);
+
+
+    
+
+    }
