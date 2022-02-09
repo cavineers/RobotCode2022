a1701c5eae8bd832fa1f872aad51cb07a13e955d
POODLEMASTER1 <91638186+POODLEMASTER1@users.noreply.github.com>
2022-02-09 18:37:45 -0500

    Motor for intake
    We did it By Bryan Kit Bode Caroline and Charles

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,8 @@
 package frc.robot;
 
 import frc.robot.commands.AutoShoot;
+import frc.robot.commands.ToggleIntake;
+import frc.robot.commands.ToggleIntakeTwo;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
--- src/main/java/frc/robot/commands/ToggleIntake.java
+++ src/main/java/frc/robot/commands/ToggleIntake.java
@@ -2,38 +2,46 @@
 
 import frc.robot.Robot;
 import frc.robot.commands.ToggleIntake;
-import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.subsystems.Intake;
-import edu.wpi.first.wpilibj.DigitalInput;
 
 
 
 public class ToggleIntake extends CommandBase {
    
-public ToggleIntake() {
-    this.addRequirements(Robot.intake); 
-}
+    Joystick joystick;
 
-@Override
-public void initialize() {
-        if (Robot.intake.getDropMotorState() == Intake.IntakeMotorState.OFF) {
+    public ToggleIntake(Joystick joy) {
+        this.addRequirements(Robot.intake);
+        this.joystick = joy;
+    }
+
+    @Override
+    public void initialize() {
+        if (Robot.intake.getIntakeMotorState() == Intake.IntakeMotorState.OFF) {
             Robot.intake.setMotorState(Intake.IntakeMotorState.ON);
         } else {
             Robot.intake.setMotorState(Intake.IntakeMotorState.OFF);
-    
-}
-}
+        }
+    }
 
-@Override
+    @Override
     public void execute() {}
 
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+
+    }
 
     @Override
     public boolean isFinished() {
-        return true;
-}
+        if (Robot.intake.getSensorOneState() == true) {
+            return true;
+        } else if (this.joystick.getRawButtonPressed(1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
