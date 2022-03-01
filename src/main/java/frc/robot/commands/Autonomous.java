1d8b096e97e37f1afb88375f15f5a319e82c9f39
Dylan Meacham <91638140+DylanTMeacham@users.noreply.github.com>
2022-03-01 10:40:23 -0500

    Update Autonomous.java

--- src/main/java/frc/robot/commands/Autonomous.java
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
 import frc.robot.RobotContainer;
 
 public class Autonomous extends CommandBase {
@@ -20,54 +21,50 @@ public class Autonomous extends CommandBase {
 
     @Override
     public void execute() { 
-//Drives backwards for 5 seconds and enables shooter/stops and disables shooter
         if(this.startTime >= 0){
             this.rc.drivetrain.drive(-0.3, 0, true);
-            // enable shooter
-            this.rc.m_autoShoot.schedule();
-        } else if (this.startTime <= 5){
+            this.rc.m_autoShoot.execute();
+        } else if (this.startTime <= 3){
             this.rc.drivetrain.drive(0, 0, true);
-            //disable shooter
-            this.rc.m_autoShoot.cancel();
+            this.rc.m_autoShoot.end(true);
         }
-//Go to closest (teams) cargo, toggle intake ON, enable shooter
-        if(this.startTime >= 6){
+
+        if(this.startTime >= 3.05){
             this.rc.drivetrain.drive(0, 0, true); 
-        } else if (this.startTime <= 6.5){
+        } else if (this.startTime <= 3.5){
+            this.rc.drivetrain.drive(0.3, 0, true);
+        }
+        if(this.startTime >= 4){
+            this.rc.drivetrain.drive(0, 0.1, true);// Turns Right
+            //this.rc.drivetrain.drive(0, -0.1, true);//Turns Left 
+        } else if (this.startTime <= 4.5){
             this.rc.drivetrain.drive(0, 0, true);
         }
-        if(this.startTime >= 7.5){
-            this.rc.drivetrain.drive(0, 0.1, true);//Turns right
-            //this.rc.drivetrain.drive(0, -0.1, true);//Turns left
-        } else if (this.startTime <= 8){
-            this.rc.drivetrain.drive(0, 0, true);
-        }
-        if (this.startTime >= 8.5){
+        
+        if (this.startTime >= 5){
             this.rc.drivetrain.drive(-0.3, 0, true);
-        } else if (this.startTime <= 10){
+        } else if (this.startTime <= 0.5){
             this.rc.drivetrain.drive(0, 0, true);
             this.rc.m_lowerIntake.schedule();
             this.rc.m_toggleIntake.schedule();
-        } else if(this.startTime >= 11){
-            this.rc.m_lowerIntake.cancel();
-            this.rc.m_toggleIntake.cancel();
-        }
-        if (this.startTime >= 13){
-            this.rc.m_autoShoot.schedule();
-        } else if (this.startTime >= 14){
-            this.rc.m_autoShoot.cancel();
-        }
-       // if all else fail (Fnaf.Lore.explain){
 
+        } else if(this.startTime >= 6){
+        this.rc.m_toggleIntake.cancel();
+        this.rc.drivetrain.drive(-0.3, 0, true);
+        this.rc.m_autoShoot.schedule();
+        }
     }
 
     @Override
     public void end(boolean interrupted) {
         this.rc.drivetrain.drive(0, 0, true);
+        this.rc.m_autoShoot.cancel();
+        this.rc.m_lowerIntake.cancel();
+        this.rc.m_toggleIntake.cancel();
     }
 
     @Override
     public boolean isFinished() {
-        return (Timer.getFPGATimestamp()-this.startTime > 15);
+        return (Timer.getFPGATimestamp()-this.startTime > 1.5);
     }
 }
