c815ad7f6217217a313fd50d48e5c8e7cb6478eb
Dylan Meacham <91638140+DylanTMeacham@users.noreply.github.com>
2022-03-03 16:03:40 -0500

    Update Autonomous.java

--- src/main/java/frc/robot/commands/Autonomous.java
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -24,28 +24,28 @@ public class Autonomous extends CommandBase {
         if(this.startTime < 3){
             this.rc.drivetrain.drive(-0.3, 0, true);
         } else if (this.startTime >= 3 && this.startTime < 3.05){
-            this.rc.drivetrain.drive(0, 0, true);
-            this.rc.m_autoShoot.schedule();
-        } else if (this.startTime >= 3.05 && this.startTime < 5){
-            this.rc.m_autoShoot.cancel(); 
-        } else if(this.startTime >= 5 && this.startTime < 7){
+            this.rc.drivetrain.drive(0, 0, true); //driving backwards
+        } else if (this.startTime >= 3.05 && this.startTime < 7){
+            this.rc.m_autoShoot.schedule(); //bot revs up to shoot and shoots
+        } else if(this.startTime >= 7 && this.startTime < 7.5){
+            this.rc.m_autoShoot.cancel(); // Now weve shoot the perloaded ball and it slows down
             //Change these based on where we are placed at start of match
             this.rc.drivetrain.drive(0, 0.1, true);// Turns Right
             //this.rc.drivetrain.drive(0, -0.1, true);//Turns Left
             //this.rc.drivetrain.drive(-0.3, 0, true); //Drive  
-        } else if (this.startTime >= 7 && this.startTime < 7.5){
-            this.rc.drivetrain.drive(0.3, 0, true);
-        } else if (this.startTime >= 7.5 && this.startTime < 9){
+        } else if (this.startTime >= 8 && this.startTime < 10){
+            this.rc.drivetrain.drive(0.3, 0, true); //driving toward next ball
+        } else if (this.startTime >= 7.5 && this.startTime < 9){ //we lower the intake 
             this.rc.drivetrain.drive(0, 0, true);
-            this.rc.m_lowerIntake.schedule();
+            this.rc.m_lowerIntake.schedule(); 
+        } else if(this.startTime >= 9 && this.startTime < 11){//we pick up the ball
             this.rc.m_toggleIntake.schedule();
-        } else if(this.startTime >= 9 && this.startTime < 11){
-            this.rc.m_toggleIntake.cancel();
-            this.rc.drivetrain.drive(-0.3, 0, true);
+    
+        } else if (this.startTime >= 11.5 && this.startTime < 13){//we turn
+            this.rc.drivetrain.drive(0, 0.1, true);
+        } else if (this.startTime >= 13.5 && this.startTime < 15){//we shoot the ball(we sit on the toile-)
             this.rc.m_autoShoot.schedule();
-        } else if (this.startTime >= 11.5 && this.startTime < 13){
-            this.rc.m_autoShoot.cancel();
-        } else if(this.startTime == 15){
+        } else if(this.startTime == 15){//stopping everything
             this.rc.drivetrain.drive(0, 0, true);
             this.rc.m_autoShoot.cancel();
             this.rc.m_toggleIntake.cancel();
